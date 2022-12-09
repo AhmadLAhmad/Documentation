d2c2d449a7dd88bdd1f72325ce889ed9f8b182ed
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2022-12-09 13:13:26 +0200

    finished step4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 4/Step_4.js
@@ -0,0 +1,21 @@
+// function check(){
+// var x=document.
+
+
+// }
+var password = document.querySelector("#password");
+ var confirm = document.querySelector("#confirmation");
+ var button = document.querySelector("button");
+
+ var change_border = function() {
+
+ if(password.value != confirm.value) {
+    password.style.border = "3px solid red";
+    confirm.style.border = "3px solid red";
+   }else 
+   {
+    password.style.border = "";
+    confirm.style.border = " ";
+   }
+  }
+ button.addEventListener("click", change_border);
