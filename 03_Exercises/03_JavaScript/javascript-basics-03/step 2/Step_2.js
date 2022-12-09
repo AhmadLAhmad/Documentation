486fe1f0b75660da093258d8cb315e57248ffa9e
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2022-12-09 09:53:46 +0200

    finished step3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+document.getElementById("poo").style.display="none";
+
+}
+function myFunction2() {
+    document.getElementById("poo").style.display="";
+    
+    }
---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,11 @@
+function green(){
+    document.getElementById("text").style.color="green";
+}
+function red()
+{
+document.getElementById("text").style.color="red";
+}
+function blue()
+{
+document.getElementById("text").style.color="blue";
+}
