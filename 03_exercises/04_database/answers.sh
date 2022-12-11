c0229593e083601ef51bbf896d1528657271449a
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2022-12-11 14:40:36 +0200

    Creating Table

--- 03_exercises/04_database/answers.sh
+++ 03_exercises/04_database/answers.sh
@@ -1,3 +1,4 @@
+*Basic Queries*
 1- select Name from students
 2- select * from students where Age > 30
 3- select name from students where Gender='F' and Age > 30
@@ -5,3 +6,18 @@
 5- insert into students(ID,Name,Age,Gender,Points) values(7,"Ahmad","24","M",1000)
 6- update students set Points=500 where ID=2
 7- update students set points=100 where ID=1
+
+*Creating Table*
+10- INSERT INTO graduates (ID,name, Age, Gender,Points)
+    SELECT 	ID,name,Age, Gender,Points
+    FROM students
+    WHERE ID=4
+
+11- UPDATE graduates
+    SET Graduation ='08/09/2018'
+    WHERE ID = 4
+
+12- DELETE FROM students WHERE ID=4
+
+
+
