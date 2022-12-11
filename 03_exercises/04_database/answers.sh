fde1d88451ad6c8930a5def2f6e45c08e561ff42
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2022-12-11 13:17:02 +0200

    finished Basic Queries

---
+++ 03_exercises/04_database/answers.sh
@@ -0,0 +1,7 @@
+1- select Name from students
+2- select * from students where Age > 30
+3- select name from students where Gender='F' and Age > 30
+4- select name,points from students where name='Alex'
+5- insert into students(ID,Name,Age,Gender,Points) values(7,"Ahmad","24","M",1000)
+6- update students set Points=500 where ID=2
+7- update students set points=100 where ID=1
