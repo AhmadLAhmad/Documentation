417c7a416ca1773d376ce8682792b73b3a9246ef
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2023-01-08 04:20:10 +0200

    List function

--- 04_Challenges/NodeJS-Challenge/tasks.js
+++ 04_Challenges/NodeJS-Challenge/tasks.js
@@ -50,6 +50,32 @@ const addFunction = () => {
   }
 };
 
+/* list function */
+
+const listFunction = () => {
+
+  let data = [];
+
+  const fileData = fs.readFileSync(currentWorkingDirectory +'todo.txt').toString();
+
+  data = fileData.split('\n');
+
+  let filterData = data.filter(function(value) 
+  {
+      return value !== '';
+  });
+
+  if (filterData.length === 0) 
+  {
+      console.log('There are no pending todos!');
+  }
+  for (let i = 0; i < filterData.length; i++) 
+  {
+      console.log((filterData.length - i) + '. ' +filterData[i]);
+  }
+};
+
+
 
 
 
@@ -65,6 +91,11 @@ switch (args[2]) {
             addFunction();
             break;
         }
+  case 'ls':
+          {
+            listFunction();
+            break;
+          }
 
     default:
         {
